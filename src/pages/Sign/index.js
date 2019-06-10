8df7b5143d3c2b1d677c80dc615712278860771e
unknown <962030347@qq.com>
2019-06-10 21:20:57 +0800

    feat(master): add sign page bg

--- src/pages/Sign/index.js
+++ src/pages/Sign/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Flamingo } from '../../assets';
+import './index.less';
 
 export default (porps) => {
-    return (<div>
-
+    return (<div className="sign-wrapper" style={{ backgroundImage: `url(${Flamingo})` }}>
+        <div className="box"></div>
     </div>);
 }
