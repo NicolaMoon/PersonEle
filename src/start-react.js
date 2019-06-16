7669f71ba5d34f6cf4c5b1dc0edce81d10eeb2f3
unknown <962030347@qq.com>
2019-06-16 22:16:04 +0800

    feat(master): add websocket to connect server

--- src/start-react.js
+++ src/start-react.js
@@ -1,5 +1,8 @@
 const net = require('net')
 const childProcess = require('child_process')
+const io = require('socket.io-client')
+const PATH = 'http://127.0.0.1:8080/';
+const socket = io.connect(PATH);
 
 const port = process.env.PORT ? process.env.PORT - 100 : 3000
 
@@ -14,6 +17,7 @@ const tryConnection = () => {
     () => {
       client.end()
       if (!startedElectron) {
+        connectServer();
         console.log('starting electron')
         startedElectron = true
         const exec = childProcess.exec
@@ -28,3 +32,13 @@ tryConnection()
 client.on('error', () => {
   setTimeout(tryConnection, 1000)
 })
+
+const connectServer = () => {
+  socket.on('connect', () => {
+    console.log(`[SUCCESS]-connect to server`);
+  });
+}
+
+socket.on('disconnect', () => {
+  socket.open();
+});
